365b9970df286367abbe21eaab40c7627529a2ae
khan ashiq <mdaashiquippz@gmail.com>
2023-04-04 17:54:20 +0000

    tex amount fix

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    const {img, name, seller, ratings,price} =props.product
+    console.log(props)
+    const {img, name, seller, ratings,price} =props.product;
+    const handleAddToCart= props.handleAddToCart;
+    
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +17,10 @@ const Product = (props) => {
             <p>Manufacturer {seller}</p>
             <p>Ratings {ratings}</p>
             </div>
-            <button className='btn-cart'>Add to card</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to card
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
 
         </div>
     );
